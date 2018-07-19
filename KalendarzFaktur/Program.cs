2328bee05dc2cdb901369ee1ff6187467d9593a2
Conrad Niedzwiedzki <kontakt@conradniedzwiedzki.pl>
2018-07-19 23:50:07 +0200

    Added simple encryption

--- KalendarzFaktur/Program.cs
+++ KalendarzFaktur/Program.cs
@@ -1,18 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Windows.Forms;
 
 namespace KalendarzFaktur
 {
     static class Program
     {
+        const string _sciezkaDoZapisanychFaktur = "faktury.json";
+        const string _sciezkaDoBazyPodpowiedziFirm = "firmy.json";
         [STAThread]
         static void Main()
         {
+            File.Decrypt(_sciezkaDoZapisanychFaktur);
+            File.Decrypt(_sciezkaDoBazyPodpowiedziFirm);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new KalendarzFakturWindow());
+            File.Encrypt(_sciezkaDoZapisanychFaktur);
+            File.Encrypt(_sciezkaDoBazyPodpowiedziFirm);
         }
     }
 }
